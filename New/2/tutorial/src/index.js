ef22d4c8755cc101a0259c0f2ded7c3340f2c3ad
Miraj Khandaker <mirajkhandaker@gmail.com>
2020-03-13 08:10:34 +0600

    push 2

--- New/2/tutorial/src/index.js
+++ New/2/tutorial/src/index.js
@@ -1,8 +1,49 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import './index.css';
 
-function Greetings(){
-    return <h1>hello greetings</h1>
+function BookS(){
+    return (
+        <section className="books" >
+            <Book />
+            <Book />
+            <Book />
+            <Book />
+            <Book />
+            <Book />
+            <Book />
+            <Book />
+            <Book />
+            <Book />
+            <Book />
+            <Book />
+        </section>
+    );
 }
 
-ReactDom.render(<Greetings />,document.getElementById('root'));
+
+    const Book = () => {
+        return (
+            <article className="book">
+                <BookImage />
+                <Title />
+                <Author />
+            </article>
+        );
+    }
+
+
+const authorDesign = {
+    color:'green',
+    fontSize:'14px',
+    letterSpacing:'10px'
+}
+
+const BookImage = () => {
+	return <img src="https://m.media-amazon.com/images/I/51TNUIyRjoL._AC_UY218_ML3_.jpg" alt="F*ck I'm Bored! Activity Book For Adults" />
+}
+
+const Title = () => <h1 style={{fontSize:'20px',color:'red'}}>F*ck I'm Bored! Activity Book For Adults</h1>
+const Author = () => <h2 style={authorDesign}>by Tamara L Adams</h2>
+
+ReactDom.render(<BookS />,document.getElementById('root'));
